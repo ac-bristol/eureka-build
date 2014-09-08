7ad7f5dcb06e7fc96d2ee70d51067736105753f4
ac-bristol <andrewcocker86@googlemail.com>
2014-09-08 16:19:02 +0100

    initial files commited

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+  var jsFileList = [
+    '<%= meta.srcPath %>javascript/libs/jquery.1.11.1.js',
+    '<%= meta.srcPath %>javascript/libs/respond.min.js'
+  ];
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      srcPath: 'src/',
+      buildPath: 'assets/',
+      projectName: '',
+      projectNameSpace: '',
+    },
+    concat: {
+      options: {
+        stripBanners: false,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      dist: {
+        src: [jsFileList],
+        dest: '<%= meta.buildPath %>javascript/build.js',
+      },
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: '<%= meta.srcPath %>javascript/main.js',
+        dest: '<%= meta.buildPath %>javascript/main.min.js'
+      }
+    },
+    sprite:{
+      all: {
+        src: 'src/images/sprites/*.png',
+        destImg: 'assets/images/sprite.png',
+        destCSS: 'src/scss/_sprites.scss',
+        'cssFormat': 'scss',
+      }
+    },
+    compass: {
+      dev: {
+          options: {              
+            sassDir: '<%= meta.srcPath %>scss',
+            cssDir: '<%= meta.buildPath %>styles',
+            imagesDir: '<%= meta.buildPath %>images',
+            javascriptsDir: '<%= meta.buildPath %>scripts',    
+            fontsDir: '<%= meta.buildPath %>fonts',
+            outputStyle: 'expanded',
+            sourcemap: true
+          }
+      },
+      build: {
+        options: {              
+          sassDir: '<%= meta.srcPath %>scss',
+          cssDir: '<%= meta.buildPath %>styles',
+          imagesDir: '<%= meta.buildPath %>images',
+          javascriptsDir: '<%= meta.buildPath %>scripts',    
+          fontsDir: '<%= meta.buildPath %>fonts',
+          outputStyle: 'compressed',
+          environment: 'production'
+        }
+      },
+    },
+    watch: {
+      uglify: {
+        files: ['<%= meta.srcPath %>javascript/main.js'],
+        tasks: ['uglify'],
+      },
+      sass: {
+        files: ['<%= meta.srcPath %>scss/*.scss'],
+        tasks: ['compass:dev'],
+      },
+      sprite: {
+      	files: ['<%= meta.srcPath %>images/sprites/*.png'],
+      	tasks: ['sprite']
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-spritesmith');
+
+  // Default task.
+  grunt.registerTask('development', ['compass:dev','concat', 'sprite']);
+
+  // Build Task
+  grunt.registerTask('build', ['compass:build','uglify', 'concat', 'sprite']);
+
+};
--- README.md
+++ README.md
@@ -1,4 +1,38 @@
 eureka-build
 ============
 
-Base build project utilising the latest web technologies inc. grunt controlling sass, javascript, image spriting
+An easy to install, base build project utlising NPM and Grunt, Sass, Compass, Image spriting<br />
+JS hint, Uglify and concat.
+
+Getting started!
+================
+
+1. Make sure you have latest version of Compass installed. If its not installed, visit the website<br />
+for more details: http://compass-style.org/install/
+
+2. Install node dependencies using the following command:<br />
+<code>npm install</code>
+
+3. The <code>src</code> folder is where all working styles/scripts/png images will be placed.<br /> 
+The <code>assets<code> folder is where all compiled code will be dropped. This is the only<br />
+folder you will need when deploying a site using <strong>eureka-build</strong>.
+
+4. To compile your code, run one of the following commands:<br />
+
+<code>grunt development</code><br />
+<code>grunt build</code><br />
+
+5. The watch command will look for changes made to your styles, scripts or pngs placed <br />
+in the sprite folder:
+
+<code>grunt watch</code><br /><br />
+Use <code>ctrl+c</code> to cancel this command<br /><br />
+
+6. To add more js libraries, drop them into the <code>libs</code> folder and then add them to the<br />
+grunt file in the <code>jsFileList</code>  array.
+
+
+=================
+Thats it! Have fun and feel free to suggest more useful web development libraries
+
+
